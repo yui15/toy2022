a5ed216fd221b07feae659ef7b5f444a8e4207e7
yui15 <fumufumu1510@gmail.com>
2022-07-04 13:39:59 +0900

    add Toy.hs

--- src/Toy.hs
+++ src/Toy.hs
@@ -1,2 +1,37 @@
 module Toy where
 
+type SourceCode = String
+type Interactive = [Input] -> [Output]
+type Input  = String
+type Output = String
+
+drive :: Interactive -> (String -> String)
+drive f = unlines . f . lines
+
+toy :: SoruceCode -> Interactive
+toy prog inputs = map output . eval . initState prog inputs))
+
+type Memory = ()
+
+load :: SourceCode -> Memory
+load = undefined
+
+initState :: Memory -> ([Input] -> ToyState)
+initState mem inputs = undefined
+
+output :: Toystate -> Output
+output state = undefined
+
+type Toystate = ()
+
+eval :: ToyState -> [ToyState]
+eval state = state: rests
+    where
+        rests | isFinal state = []
+              | otherwise     = eval [step state]
+
+isFinal :: Toystate -> Bool
+isFinal state = undefined
+
+step :: ToyState -> ToyState
+step state = undefined
